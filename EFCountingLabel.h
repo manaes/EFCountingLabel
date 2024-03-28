930206e6e198e660ffe5abc9a5e882e331a196e0
wanny.park <wanny.park@pandora.tv>
2024-03-28 12:29:03 +0900

    carthage 설정 추가

---
+++ EFCountingLabel.h
@@ -0,0 +1,18 @@
+//
+//  EFCountingLabel.h
+//  EFCountingLabel
+//
+//  Created by manaes on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EFCountingLabel.
+FOUNDATION_EXPORT double EFCountingLabelVersionNumber;
+
+//! Project version string for EFCountingLabel.
+FOUNDATION_EXPORT const unsigned char EFCountingLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EFCountingLabel/PublicHeader.h>
+
+
